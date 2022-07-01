a4e589dfdb5eed801c9aec71968c8fca3bbd02d0
marjsky <67877013+marjsky@users.noreply.github.com>
2022-06-30 21:56:41 -0400

    add  JSON of ten movie route to /movie

--- index.js
+++ index.js
@@ -2,3 +2,50 @@ const express = require('express'),
     morgan = require('morgan');
 
 const app = express();
+
+let topTenMovies = [
+    {
+        title: 'Hancock',
+        genre: 'Action/Fantasy'
+    },
+    {
+        title: 'The Way Back',
+        genre: 'Sport/Drama'
+    },
+    {
+        title: 'Gamer',
+        genre: 'Action/Sci-fi'
+    },
+    {
+        title: 'Divergent',
+        genre: 'Action/Sci-fi'
+    },
+    {
+        title: 'The Man from Toronto',
+        genre: 'Action/Comedy'
+    },
+    {
+        title: 'Mars Attacks!',
+        genre: 'Sci-fi/Comedy'
+    },
+    {
+        title: 'Drumline: A New Beat',
+        genre: 'Drama/Comedy music'
+    },
+    {
+        title: 'Zygote',
+        genre: 'Short/Sci-Fi Horror'
+    },
+    {
+        title: 'Aquaman',
+        genre: 'Action/Adventure'
+    },
+    {
+        title: 'Life',
+        genre: 'Sci-fi/Horror'
+    }
+];
+
+app.get('/movie', (req, res) => {
+    res.json(topTenMovies);
+});
