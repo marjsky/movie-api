8a552d584a5a5528f38dc05edacbf3f4b93178c5
marjsky <67877013+marjsky@users.noreply.github.com>
2022-07-04 02:07:33 -0400

    install npm packages

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    "linebreak-style": 0,
+    "error": { allow: ["warn"] }
+  }
+}
