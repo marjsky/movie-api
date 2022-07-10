e825a8101c4ecb561b43ad5f548c4e0303d48d88
marjsky <67877013+marjsky@users.noreply.github.com>
2022-07-10 18:21:21 -0400

    first test default res. at /

--- index.js
+++ index.js
@@ -21,8 +21,30 @@ app.use(bodyParser.json()); //Returns middleware parses json looks at requests t
 
 app.use(bodyParser.urlencoded({ extended: true })); // Returns middleware parses urlencoded bodies 
 
+// access documentation.html using express.static
+app.use("/documentation", express.static("public"));
 
-  
+//morgan request logger
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, 'log.txt'), {flags: 'a+'}
+)
+app.use(morgan('combined', {stream: accessLogStream}));
+
+//error handing
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Error");
+})  
+
+
+// default text response when at /
+app.get("/", (req, res) => {
+  res.send("Welcome to DaFlix!");
+});
+
+
+
+
 
 app.listen(8080, () => {
   console.log('Server started on port 8080; press Ctrl-C to terminate...');
