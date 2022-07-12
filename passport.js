bf76e6e02acb6ce280ff8b6e8956dfe35e846776
marjsky <67877013+marjsky@users.noreply.github.com>
2022-07-12 09:37:24 -0400

    add code strategies in passport.js

---
+++ passport.js
@@ -0,0 +1,42 @@
+const passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy,
+    Models = require('./models.js'),
+    passportJWT = require('passport-jwt');
+
+let Users = Models.User,
+    JWTStrategy = passportJWT.Strategy,
+    ExtractJWT = passportJWT.ExtractJwt;
+
+//defines baslic HTTP authentication for login requests
+passport.use(new LocalStrategy({ //takes username and password from request body
+    usernameField: 'Username',
+    passwordField: 'Password',
+}, (username, password, callback) => {
+    console.log(username + ' ' + password);
+    Users.findOne({ Username: username }, (error, user) => { //use Mongoose to check database
+        if(error) {
+            console.log(error);
+            return callback(error);
+        }
+        if (!user) {
+            console.log('incorrect username');
+            return callback(null, false, {message: 'Incorrect username or password.'});
+        }
+        console.log('finished');
+        return callback(null, user);
+    });
+}));
+
+//JWT authentication 
+passport.use(new JWTStrategy({ //to authenticate users on JWT submitted request
+    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(), // JWT take from header of HTTP request
+    secretOrKey: 'your_jwt_secret' //to verify the signature of JWT
+}, (jwtPlayload, callback) => {
+    return Users.findById(jwtPlayload._id)
+    .then((user) => {
+        return callback(null, user);
+    })
+    .catch((error) => {
+        return callback(error)
+    });
+}));
