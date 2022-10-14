2a5d6572dd28ff301d5c2ae5f6398fa15e2f3a75
marjsky <67877013+marjsky@users.noreply.github.com>
2022-10-14 05:00:58 -0400

    remove origin of CORS

--- index.js
+++ index.js
@@ -24,18 +24,20 @@ mongoose.connect(process.env.CONNECTION_URI, {
 //Cross-Orign Resource Sharing control domans have access to API server
 const cors = require('cors');
 
-let allowedOrigins = ['*'];
+// let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234'];
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn't found on the list of allowed origins
-      let message = 'The CORS policy for this application does not allow access from origin ' + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if(!origin) return callback(null, true);
+//     if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn't found on the list of allowed origins
+//       let message = 'The CORS policy for this application does not allow access from origin ' + origin;
+//       return callback(new Error(message), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
+
+app.use(cors());
 
 app.use(bodyParser.json()); //Returns middleware parses json looks at requests the Content-Type header matches the type option.
 
