5fe8fe55907bb30e33a7683c98e489cc61d7643d
marjsky <67877013+marjsky@users.noreply.github.com>
2022-10-16 13:20:32 -0400

    add localhost:4200 to cors

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ mongoose.connect(process.env.CONNECTION_URI, {
 //Cross-Orign Resource Sharing control domans have access to API server
 const cors = require('cors');
 app.use(cors());
-let allowedOrigins = ["http://localhost:1234", "http://localhost:8080", "*"];
+let allowedOrigins = ["http://localhost:1234", "http://localhost:8080", "http://localhost:4200"];
 
 app.use(
   cors({
